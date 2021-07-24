f2f3a30c6a9682260631b92db86f3fde6ed27db1
Birger Schmidt <bs-github@netgaroo.com>
2021-07-24 11:20:22 +0200

    add copyright notice Birger Schmidt

--- software/modules/backend/enplus/enplus.h
+++ software/modules/backend/enplus/enplus.h
@@ -1,5 +1,6 @@
 /* warp-charger
  * Copyright (C) 2020-2021 Erik Fleckstein <erik@tinkerforge.com>
+ * Copyright (C)      2021 Birger Schmidt <bs-warp@netgaroo.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
